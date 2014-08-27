b7b702e0d2f40078627632ed96232240f2d9449b
dlakaplan <kaplan@uwm.edu>
2014-08-27 16:16:06 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= '6iGgz4QbYhShlJc6b1Tsx7Grhv1YlT9MB6SijjEhvoPvkigo', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [480654980], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Milwaukee Temperature', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
